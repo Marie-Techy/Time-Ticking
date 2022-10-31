4effb6fbf24cdcd9696d056737887fb22eb2c1d1
Marie-Techy <104041759+Marie-Techy@users.noreply.github.com>
2022-10-30 22:39:55 -0400

    Added Quiz Quiestions

---
+++ Developer/assests/script.js
@@ -0,0 +1,52 @@
+
+//Quiz Questions
+
+var quizQuestions = [{
+    question:"Which language runs in a web browser?",
+    choiceA: "Java",
+    choiceB: "C",
+    choiceC: "Python",
+    choiceD: "javascript",
+    correctAnswer: "d"},
+  {
+    question: "What does CSS stand for?",
+    choiceA: "Central Style Sheets",
+    choiceB: "Cascading Style Sheets",
+    choiceC: "Cascading Simple Sheets",
+    choiceD: "Cars SUVs Sailboats",
+    correctAnswer: "b"},
+   {
+    question: "What does HTML stand for?",
+    choiceA: "Hypertext Markup Language",
+    choiceB: "Hypertext Markdown Language",
+    choiceC: "Hyperloop Machine Language",
+    choiceD: "Helicopters Terminals Motorboats Lamborginis",
+    correctAnswer: "a"},
+    {
+    question: "What year was JavaScript launched?",
+    choiceA: "1996",
+    choiceB: "1995",
+    choiceC: "1994",
+    choiceD: "none of the above",
+    correctAnswer: "b"},
+    {
+    question: "When is localStorage data cleared?",
+    choiceA: "No expiration time",
+    choiceB: "On page reload",
+    choiceC: "On browser close",
+    choiceD: "On computer restart",
+    correctAnswer: "a"},  
+    {
+    question: "What does API stand for?",
+    choiceA: "Apple Pineapple IceCream",
+    choiceB: "Application Programming Interactive",
+    choiceC: "Application Programming Interface",
+    choiceD: "Automation Programming Interface",
+    correctAnswer: "c"},
+    {
+    question: "Programmers are also known as",
+    choiceA: "computers",
+    choiceB: "RAM",
+    choiceC: "class",
+    choiceD: "coders",
+    correctAnswer: "d"},
